8a766ca2e11a12ea7a70b2379435be82c163eb00
Adeline Johnson <groblewski.adeline@gmail.com>
2018-03-16 21:40:34 -0400

    added folders and files

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDCvqR9FcBRvYaHjpsfHgyQkEHC_SZhjhM",
+    authDomain: "train-scheduler-fa039.firebaseapp.com",
+    databaseURL: "https://train-scheduler-fa039.firebaseio.com",
+    projectId: "train-scheduler-fa039",
+    storageBucket: "train-scheduler-fa039.appspot.com",
+    messagingSenderId: "348552582198"
+  };
+  firebase.initializeApp(config);
+
+  
